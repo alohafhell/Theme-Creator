2acaef6aa9bf2df9fb1c02d5b18139758638c1c8
Alissa Svertokina <svertokina92@gmail.com>
2025-04-04 14:22:24 +0200

    Addlast css updates

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,17 @@ import useLocalStorageState from "use-local-storage-state";
 function App() {
   const [colors, setColors] = useLocalStorageState("colors", {
     defaultValue: initialColors,
-  }); // Color data inside state
+  }); // color data inside state
 
-  // Add new color
+  // add new color
   const addColor = (color) => {
     console.log(color);
     setColors([color, ...colors]); // Adds the new color to the list
   };
 
-  // Update existing color by matching id
+  // update existing color by matching id
   const updateColor = (updatedColor) => {
+    console.log("1111", updatedColor);
     setColors((prevColors) =>
       prevColors.map(
         (color) => (color.id === updatedColor.id ? updatedColor : color) // Updates color by matching id
@@ -25,26 +26,26 @@ function App() {
     );
   };
 
-  // Delete color by id
+  // delete color by id
   const deleteColor = (id) => {
-    // Filtering only the color with the matching id
+    // filtering only the color with the matching id
     setColors((prevColors) => {
       const updatedColors = prevColors.filter((color) => color.id !== id);
       console.log("Remaining colors after deletion:", updatedColors); // Debug log
       return updatedColors;
     });
   };
-
+  console.log(colors);
   return (
     <>
       <h1>Theme Creator</h1>
-      <ColorForm onSubmitColor={addColor} /> {/* Form to add a new color */}
+      <ColorForm onSubmitColor={addColor} /> {/* form adding a new color */}
       {colors.map((color) => (
         <Color
           key={color.id}
           color={color}
-          onUpdateColor={updateColor} // Pass update function
-          onDelete={deleteColor} // Pass delete function
+          onUpdateColor={updateColor} // pass updating function
+          onDelete={deleteColor} // pass deleting function
         />
       ))}
     </>
